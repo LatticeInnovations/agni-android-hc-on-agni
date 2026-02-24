5162d96f6fc3fd83aa5e766358cd5b5cec34bac8
Mansi Kalra <mansi.kalra23@gmail.com>
2026-02-24 09:11:56 +0530

    update heartcare id ignore code, update version code and name

--- app/src/main/java/com/heartcare/agni/data/local/enums/IdentifierIgnoreEnum.kt
+++ app/src/main/java/com/heartcare/agni/data/local/enums/IdentifierIgnoreEnum.kt
@@ -2,5 +2,5 @@ package com.heartcare.agni.data.local.enums
 
 enum class IdentifierIgnoreEnum(val value: String) {
     MEDICAL_RECORD("MR"),
-    HEARTCARE_TYPE("http://uat.heartcare.thelattice.org/dashboard/patient-info")
+    HEARTCARE_TYPE("/dashboard/patient-info")
 }
